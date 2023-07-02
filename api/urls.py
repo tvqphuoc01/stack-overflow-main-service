b69390db528442e0c2b8fee5726d35bbd7aa273b
phuoc.tran <phuoc.tran@parcelperform.com>
2023-07-02 16:26:38 +0700

    add url for noti

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from api.views.get_question import get_question_by_id, get_first_ten_question, update_question_status, delete_question
 from api.views.get_category import get_list_category
 from api.views.get_tag import get_list_tag
-
+from api.views.notification import send_notification, get_user_notification
 urlpatterns = [
     path('update-question-status', update_question_status, name='update_question_status'),
     path('delete-question', delete_question, name='delete_question'),
@@ -10,4 +10,6 @@ urlpatterns = [
     path('get-first-ten-question', get_first_ten_question, name='get_first_ten_question'),
     path('get-list-category', get_list_category, name='get_list_category'),
     path('get-list-tag', get_list_tag, name='get_list_tag'),
+    path('send-notification', send_notification, name='send_notification'),
+    path('get-user-notification', get_user_notification, name='get_user_notification'),
 ]
