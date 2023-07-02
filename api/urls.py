a278ccb8bc3aa4e759b6edf032c45c4167687de2
phuoc.tran <phuoc.tran@parcelperform.com>
2023-07-02 16:10:55 +0700

    add new api update question status

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from api.views.get_question import get_question_by_id, get_first_ten_question
+from api.views.get_question import get_question_by_id, get_first_ten_question, update_question_status
 from api.views.get_category import get_list_category
 from api.views.get_tag import get_list_tag
 
 urlpatterns = [
+    path('update-question-status', update_question_status, name='update_question_status'),
     path('get-question-by-id', get_question_by_id, name='get_question_by_id'),
     path('get-first-ten-question', get_first_ten_question, name='get_first_ten_question'),
     path('get-list-category', get_list_category, name='get_list_category'),
--- api/views/get_question.py
+++ api/views/get_question.py
@@ -51,11 +51,57 @@ def get_question_by_id(request):
 @api_view(['GET'])
 def get_first_ten_question(request):
     question_list = Question.objects.filter(question_status=True).order_by('-create_date')[:10]
+    question_list_data = []
+    
+    for question in question_list:
+        question_list_data.append(
+            {
+                "id": question.id,
+                "user_id": question.user_id,
+                "title": question.title,
+                "content": question.content,
+                "number_of_like": question.number_of_like,
+                "number_of_dislike": question.number_of_dislike,
+                "image_url": question.image_url,
+                "create_date": question.create_date,
+            }
+        )
     
     return Response(
         {
             "message": "Get question successfully",
-            "data": question_list
+            "data": question_list_data
+        },
+        status=status.HTTP_200_OK
+    )
+    
+@api_view(['PUT'])
+def update_question_status(request):
+    question_id = request.data.get('question_id')
+    question_status = request.data.get('question_status')
+    
+    if not question_id:
+        return Response(
+            {
+                "message": "Question id is required"
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+        
+    question = Question.objects.filter(id=question_id).first()
+    if not question:
+        return Response(
+            {
+                "message": "Question is not available"
+            },
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    question.question_status = question_status
+    question.save()
+    return Response(
+        {
+            "message": "Update question successfully",
         },
         status=status.HTTP_200_OK
     )
