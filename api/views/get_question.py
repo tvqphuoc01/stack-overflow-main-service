7fca0d5e6700386d8be71024d826c804354ae6b9
Trung <dotientrung201030@gmail.com>
2023-08-16 23:05:35 +0700

    update

--- api/views/get_question.py
+++ api/views/get_question.py
@@ -459,7 +459,7 @@ def get_question_by_user_id(request):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-    authen_url = "http://stack-overflow-authen-authenticator-1:8000/api/get-user-by-id"
+    authen_url = "http://stack-overflow-authen-authenticator-1:8000/api/check-user"
     response = requests.get(authen_url, params={"user_id": user_id})
     
     if (response.status_code == 200):
